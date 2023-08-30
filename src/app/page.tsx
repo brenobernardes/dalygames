79a9356dc471518bd97100da19270c04c834252d
brenobernardes <breno.pbernardes@gmail.com>
2023-08-30 15:58:01 -0300

    updating page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,22 @@ import { GameProps } from "@/utils/types/game";
 import Link from "next/link";
 import Image from "next/image";
 
+import { BsArrowRightSquare } from 'react-icons/bs';
+import { Input } from "@/components/input";
+import { GameCard } from "@/components/GameCard";
+
 async function getDalyGames() {
   try {
-    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`)
+    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`, { next: { revalidate: 320 } })
+    return res.json();
+  } catch(err) {
+    throw new Error("Failed to fetch data")
+  }
+}
+
+async function getDataGames() {
+  try {
+    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=games`, { next: { revalidate: 320 } })
     return res.json();
   } catch(err) {
     throw new Error("Failed to fetch data")
@@ -14,6 +27,7 @@ async function getDalyGames() {
 
 export default async function Home() {
   const dalyGames: GameProps = await getDalyGames();
+  const data: GameProps[] = await getDataGames();
 
   return (
     <main className='w-full'>
@@ -21,16 +35,36 @@ export default async function Home() {
         <h1 className="text-center font-bold text-xl mt-8 mb-5">Separamos um jogo exclusivo para você</h1>
         <Link href={`/game/${dalyGames.id}`}>
           <section className="w-full bg-black rounded-lg">
-            <Image 
-              src={dalyGames.image_url}
-              alt={dalyGames.title}
-              priority={true}
-              quality={100}
-              width={100}
-              height={100}
-            />
+            <div className="w-full max-h-96 h-96 relative">
+              <div className="absolute z-20 bottom-0 p-3 flex justify-center items-center gap-2">
+                <p className="font-bold text-xl text-white">{dalyGames.title}</p>
+                <BsArrowRightSquare size={24} color="#FFF" />
+              </div>
+
+              <Image 
+                src={dalyGames.image_url}
+                alt={dalyGames.title}
+                priority={true}
+                quality={100}
+                fill={true}
+                className="max-h-96 object-cover rounded-lg opacity-50 hover:opacity-100 transition-all duration-300"
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 44vw"
+              />
+            </div>
           </section>
         </Link>
+
+        <Input />
+
+        <h2 className="text-lg font-bold mt-8 mb-5">
+          Jogos para conhecer
+        </h2>
+
+        <section className="grid gap-7 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {data.map( (item) => (
+            <GameCard key={item.id} data={item} />
+          ))}
+        </section>
       </Container>
     </main>
   )
